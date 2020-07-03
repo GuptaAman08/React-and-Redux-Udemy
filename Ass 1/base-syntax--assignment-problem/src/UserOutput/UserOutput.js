17249c4db1eaf50346591182c463dad65e9a0fa5
Aman Gupta <aman.gupta@tacto.in>
2020-07-03 10:49:47 +0530

    Setting project for ass 1

---
+++ Ass 1/base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = () => {
+    return ( 
+        <div> 
+            <p> First Para </p>
+            <p> Second Para </p>
+        </div>
+    );
+}
+ 
+export default UserOutput;
